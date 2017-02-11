59b5dccc5139310a883e1174d51fa78f9de501a6
Alexandre Navarro <alexandre.j.navarro@gmail.com>
2017-02-12 00:56:45 +0100

    Add sample of event

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -7,11 +7,39 @@ class App extends React.Component {
 
 	constructor(props) {
 		super(props);
+		this.state = {employees: [{
+                                           firstName: 'Alexandre',
+                                           lastName: 'Navarro'
+                                         },
+                                         {
+                                          firstName: 'Michael',
+                                          lastName: 'Schumacher'
+                                         }], current:0};
+
+        // This binding is necessary to make `this` work in the callback
+        this.handleClick = this.handleClick.bind(this);
 	}
 
 	render() {
-		return (<div>Hello World!!!</div>)
+		return (<div>
+		Hello World!!! {this.state.employees[this.state.current].firstName}
+		<button onClick={this.handleClick}>Ok
+		</button>
+		</div>)
 	}
+
+
+    handleClick() {
+		this.setState({employees: [{
+                                           firstName: 'Alexandre',
+                                           lastName: 'Navarro'
+                                         },
+                                         {
+                                          firstName: 'Michael',
+                                          lastName: 'Schumacher'
+                                         }], current:(this.state.current + 1)%2 });
+  }
+
 }
 
 
